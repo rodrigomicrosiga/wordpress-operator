9c60e0b56807d89b5638abb728ad124cb52d8e22
rodrigomicrosiga <rodrigo.microsiga@icloud.com>
2026-05-26 20:32:43 -0300

    Requisito 4 da seção do desafio

--- api/v1alpha1/wordpresssite_types.go
+++ api/v1alpha1/wordpresssite_types.go
@@ -1,81 +1,88 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// WordpressSiteSpec defines the desired state of WordpressSite
-type WordpressSiteSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of WordpressSite. Edit wordpresssite_types.go to remove/update
+// DatabaseSpec define a configuração do banco de dados MySQL
+type DatabaseSpec struct {
+	// +kubebuilder:default:="mysql:8.0"
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Image string `json:"image,omitempty"`
+
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
+	// +kubebuilder:validation:Required
+	User string `json:"user"`
+
+	// +kubebuilder:default:="5Gi"
+	// +optional
+	StorageSize string `json:"storageSize,omitempty"`
 }
 
-// WordpressSiteStatus defines the observed state of WordpressSite.
+// WordpressSpec define a configuração da aplicação WordPress
+type WordpressSpec struct {
+	// +kubebuilder:default:="wordpress:6.5-apache"
+	// +optional
+	Image string `json:"image,omitempty"`
+
+	// +kubebuilder:default:=1
+	// +optional
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// +kubebuilder:default:="2Gi"
+	// +optional
+	StorageSize string `json:"storageSize,omitempty"`
+}
+
+// WordpressSiteSpec define o estado desejado pelo usuário (Spec)
+type WordpressSiteSpec struct {
+	// +kubebuilder:validation:Required
+	Domain string `json:"domain"`
+
+	// +kubebuilder:validation:Required
+	Wordpress WordpressSpec `json:"wordpress"`
+
+	// +kubebuilder:validation:Required
+	Database DatabaseSpec `json:"database"`
+
+	// +optional
+	IngressClassName string `json:"ingressClassName,omitempty"`
+}
+
+// WordpressSiteStatus define o estado observado pelo Operator (Status)
 type WordpressSiteStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	Phase string `json:"phase,omitempty"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+	// +optional
+	URL string `json:"url,omitempty"`
 
-	// conditions represent the current state of the WordpressSite resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
 	// +listType=map
 	// +listMapKey=type
-	// +optional
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Domain",type="string",JSONPath=".spec.domain"
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // WordpressSite is the Schema for the wordpresssites API
 type WordpressSite struct {
-	metav1.TypeMeta `json:",inline"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// metadata is a standard object metadata
-	// +optional
-	metav1.ObjectMeta `json:"metadata,omitzero"`
-
-	// spec defines the desired state of WordpressSite
-	// +required
-	Spec WordpressSiteSpec `json:"spec"`
-
-	// status defines the observed state of WordpressSite
-	// +optional
-	Status WordpressSiteStatus `json:"status,omitzero"`
+	Spec   WordpressSiteSpec   `json:"spec,omitempty"`
+	Status WordpressSiteStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -83,7 +90,7 @@ type WordpressSite struct {
 // WordpressSiteList contains a list of WordpressSite
 type WordpressSiteList struct {
 	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitzero"`
+	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []WordpressSite `json:"items"`
 }
 
